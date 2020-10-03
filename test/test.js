09ec616db4966d11d81516d0ca498c6590e07fdb
luctst <lucas.tostee@gmail.com>
2020-10-03 15:40:48 +0200

    Fix  errors

--- test/test.js
+++ test/test.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 5e076af... Start writing tests
 const test = require("ava");
 const browserEnv = require("browser-env");
 let Leo;
@@ -10,28 +6,12 @@ browserEnv(['window', 'document']);
 
 test.before(function () {
     Leo = require("../lib/main");
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     document.querySelector("body").appendChild(document.createElement("main"));
 })
 
 test('Options should be an array', function (t) {
     const error = t.throws(() => new Leo("main", {}), {
-=======
-})
-
-test('Options should be an array', function (t) {
-    const error = t.throws(() => new Leo({}), {
->>>>>>> 5e076af... Start writing tests
-=======
-
-    document.querySelector("body").appendChild(document.createElement("main"));
-})
-
-test('Options should be an array', function (t) {
-    const error = t.throws(() => new Leo("main", {}), {
->>>>>>> 1511d91... Fix CI errors
         instanceOf: Error,
     });
 
@@ -39,25 +19,9 @@ test('Options should be an array', function (t) {
 });
 
 test('Options data should be objects', function (t) {
-<<<<<<< HEAD
-<<<<<<< HEAD
     const e = t.throws(() => new Leo("main", [""]), {
-=======
-    const e = t.throws(() => new Leo([""]), {
->>>>>>> 5e076af... Start writing tests
-=======
-    const e = t.throws(() => new Leo("main", [""]), {
->>>>>>> 1511d91... Fix CI errors
         instanceOf: Error
     });
 
     t.is(e.message, 'Options only accepts objects');
-<<<<<<< HEAD
 });
-=======
-const ava = require("ava");
-ava.todo("Implement test");
->>>>>>> 448f9eb... Make test script passed
-=======
-});
->>>>>>> 5e076af... Start writing tests
